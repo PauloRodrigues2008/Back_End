4864a978c715ed1dba44c762bdbb4c0faad76f15
paulo <pauloandrerodriguesdasilva456@gmail.com>
2025-10-23 08:56:47 -0300

    Lista de atividades Back_End

---
+++ ReviewArrys/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//cria um arrys tamanho 4 e preenche com vazio
+string[] nomes = new string[4];
+
+nomes[0] = "Maria";
+nomes[1] = "Matheus";
+nomes[2] = "Eloisa";
+nomes[3] = "Felipe";
+
+Console.WriteLine();
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine(nomes[i]);
+}
